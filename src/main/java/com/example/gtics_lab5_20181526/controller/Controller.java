4935e252c373970ab61da3b74f1d3ddb32252d53
JACORVI <117043793+JACORVI@users.noreply.github.com>
2024-05-08 20:21:36 -0500

    2do
    2do

--- src/main/java/com/example/gtics_lab5_20181526/controller/Controller.java
+++ src/main/java/com/example/gtics_lab5_20181526/controller/Controller.java
@@ -3,8 +3,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
-    @GetMapping(value = "/")
+    @GetMapping(value = "/Vista_Inicio")
     public String gticscontroller(){
         return "Vista_Inicio";
+    }
+    @GetMapping(value = "/Vista_Inicio/Gestion_Tecnicos")
+    public String gticstecnico(){
+        return"Vista_Inicio/Gestion_Tecnicos";
     }
 }
---
+++ src/main/java/com/example/gtics_lab5_20181526/entity/Tecnicos.java
@@ -0,0 +1,15 @@
+package com.example.gtics_lab5_20181526.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+@Entity
+@Getter
+@Setter
+@Table(name='')
+public class Tecnicos {
+    @ModelAttribute
+
+}
