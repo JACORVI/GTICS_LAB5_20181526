de41c05ba33e5daf90b3bd2a335fac8f79a77c92
JACORVI <117043793+JACORVI@users.noreply.github.com>
2024-05-08 19:27:31 -0500

    Commit 1
    primer commit

---
+++ src/main/java/com/example/gtics_lab5_20181526/GticsLab520181526Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab5_20181526;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab520181526Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GticsLab520181526Application.class, args);
+    }
+
+}
